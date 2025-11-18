63efba305db3d709055948753751fbe9dbbdb153
Kartik <Kartik110605@gmail.com>
2025-11-18 12:38:03 +0530

    forked the repo from the aarab Nischal

---
+++ src/constant/experience.ts
@@ -0,0 +1,27 @@
+export const experienceData = [
+  {
+    role: "Summer Intern",
+    year: "Jun'25 - Jul'25",
+    description: [
+      "Generated synthetic wireless datasets using DeepMIMO (for zigzag indoor mobility) and NYUSIM (for mobile user propagation) to simulate realistic 5G channel conditions.",
+      "Preprocessed the dataset to create a novel image-based dataset of amplitude and phase components for ML-driven channel prediction and zero-feedback beamforming.",
+      "Optimized datasets for DL models like CNN, LSTM, and LFS-GAN targeting adversarially robust zero-feedback beamforming and future channel estimation under dynamic scenarios",
+      "Conducted exploratory research on Layer 3 and cross-layer attacks in 5G OAI-ORAN environments to evaluate the resilience of our custom SECSM agent",
+    ],
+    company: "Malaviya National Institure of Technology Jaipur",
+    technologies: [
+    ],
+  },
+  {
+    role: "Research Intern - Remote",
+    year: "May'25 - Jul'25",
+    description: [
+      "Designed and implemented a Streamlit-based web application leveraging AI to automate and optimize exam seating allocation, enhancing efficiency for large-scale academic events.",
+      "Developed algorithms for course-wise student distribution, dynamic buffer seat management, and sparse/dense seating configurations using predictive modeling and AI-driven logic.",
+      "Engineered solutions for classroom clash detection, faculty movement optimization, and adaptive seating plans to maximize resource utilization during examinations.",
+      "Generated comprehensive outputs such as master seating plans, unallocated lists, and auto-formatted attendance sheets compatible with Excel, integrating real-time data processing and reporting.",
+    ],
+    company: "Indian Institute of Technology Patna",
+    technologies: ["Streamlit", "Python", "Data Analysis"],
+  },
+];
---
+++ src/constant/self.ts
@@ -0,0 +1,40 @@
+export const selfData = {
+  name: "Kartik Maheshwari",
+  first_name: "Kartik",
+  last_name: "Maheshwari",
+  jobTitle: "Aspiring Software Engineer",
+  workFor: "RTU Kota",
+  current_location: {
+    city: "Kota",
+    state: "Rajasthan",
+    country: "India",
+  },
+  DOB: {
+    dd: 11,
+    mm: 8,
+    yyyy: 2005,
+  },
+  email: "contact.mkartik@gmail.com",
+  roles: [
+    "Aspiring Software Engineer",
+    "Tech Enthusiast",
+    "ML Enthusiast",
+    "Android Developer",
+    "Problem Solver",
+  ],
+  desc: "I design and build tools that live on the internet.",
+  bio: "I'm a student developer passionate about building smart, connected applications. From crafting intuitive Android apps to exploring the complexities of networking and machine learning, I love turning complex ideas into functional, real-world solutions.",
+  about: [
+    "Hey there! I’m an aspiring software engineer with a knack for turning curiosity into code. My journey into tech started with simple “how does this work?” moments—and before I knew it, I was diving deep into the worlds of machine learning, computer networking, and Android development.",
+    "I’m fascinated by how machines talk, learn, and make decisions. These days, I spend more time understanding network flows and tinkering with ML ideas—chasing the little aha! moments that keep me hooked on engineering.",
+    "I'm currently on a quest to understand intelligent systems and large-scale data flow. When I'm not coding, I'm probably drawing network diagrams on a whiteboard, reading ML papers I barely understand (yet!), or trying to automate my own life. I love building things that aren't just functional, but smart and scalable.",
+    "If you’re into tech, ideas, or just good conversations, let explore together"
+  ],
+
+  socials_username: {
+    github: "kartik23411",
+    instagram: "kartik_11605",
+    leetcode: "Kartik1165",
+    linkedin: "kartik411",
+  },
+};
